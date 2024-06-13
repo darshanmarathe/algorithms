f544afc9ae47c3ccbd6babe5b27d94bb6a555ccd
YourUsername <your@email.com>
2024-06-13 10:46:13 +0000

    added commit file

--- commit.sh
+++ commit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Set your Git username and email
-GIT_USERNAME="YourUsername"
-GIT_EMAIL="your@email.com"
 
 # Check if a commit message is provided
 if [[ $# -eq 0 ]]; then
@@ -17,9 +14,6 @@ git add .
 COMMIT_MESSAGE="$1"
 git commit -m "$COMMIT_MESSAGE"
 
-# Set the Git user details
-git config user.name "$GIT_USERNAME"
-git config user.email "$GIT_EMAIL"
 
 # Push changes to the remote repository (assuming 'origin' as the remote)
 git push origin master
