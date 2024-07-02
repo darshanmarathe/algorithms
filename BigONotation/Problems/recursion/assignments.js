16da27583ef23b7f91ff7a405768187067e1e619
YourUsername <your@email.com>
2024-07-02 14:20:54 +0000

    Added power example

--- BigONotation/Problems/recursion/assignments.js
+++ BigONotation/Problems/recursion/assignments.js
@@ -1,4 +1,13 @@
 const log = console.log;
 import Test from "../../../Uitls/test.js";
 console.clear();
-var t = new Test("Assignment No :: 1 :: ");
+var t = new Test("Assignment No :: 1 :: Power ");
+
+log(power(2, 0)); // 1
+log(power(2, 2)); // 4
+log(power(2, 4)); // 16
+
+function power(num1, num2) {
+  if (num2 === 0) return 1;
+  return num1 * power(num1, num2 - 1);
+}
