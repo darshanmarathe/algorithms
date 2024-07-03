f3dd4f6f15a4740c90a92ee37f8783d5c32c7985
Darshan Marathe <darshanmarathe@gmail.com>
2024-07-03 09:21:56 +0530

    work in progrss

--- BigONotation/Problems/recursion/assignments.js
+++ BigONotation/Problems/recursion/assignments.js
@@ -14,3 +14,10 @@ function power(num1, num2) {
 t.that(power(2, 0), `power(2, 0)`).isEquals(1); // 1
 t.that(power(2, 2), `power(2, 2)`).isEquals(4); // 4
 t.that(power(2, 4), `power(2, 4)`).isEquals(16); // 16
+
+t.Reset('Assignment No :: 2 ::  ')
+t.Reset('Assignment No :: 3 ::  ')
+t.Reset('Assignment No :: 4 ::  ')
+t.Reset('Assignment No :: 5 ::  ')
+t.Reset('Assignment No :: 6 ::  ')
+t.Reset('Assignment No :: 7 ::  ')
