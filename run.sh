9ddcd08c3816abc7b6807bbd41b1bbc286af186a
YourUsername <your@email.com>
2024-07-09 06:52:59 +0000

    added the run files for windows and mac

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if nodemon is installed globally
+if npm list -g | grep -q "nodemon"; then
+    echo "nodemon is already installed globally."
+else
+    echo "nodemon is not installed globally. Installing..."
+    npm install -g nodemon
+fi
+
+# Run nodemon with the specified file
+nodemon BigONotation/Problems/recursion/assignments.js
