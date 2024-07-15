d1a21aad0e44e255a8c4cbf2b93dbbbe6e1993bb
YourUsername <your@email.com>
2024-07-15 08:59:35 +0000

    binary search completed

--- BigONotation/Problems/search/binarySearch.js
+++ BigONotation/Problems/search/binarySearch.js
@@ -20,44 +20,39 @@ function binarySearchNum(arr = [], target = 0) {
   }
 }
 
-function binarySearchNumIndex(arr = [], target = 0, index = -1) {
-  const len = arr.length;
-  if (len === 0) return -1;
-  if (len === 1) return arr[0] === target ? index : -1;
-  const mid = Math.ceil(len / 2);
+function BinarySearch(arr = [], elem = 0) {
+  let len = arr.length;
+  let start = 0;
+  let end = len - 1;
+  let middle = Math.floor((start + end) / 2);
 
-  if (arr[mid] === target) return index;
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid, len);
-
-  if (target > left[left.length - 1]) {
-    return binarySearchNumIndex(right, target);
-  } else {
-    return binarySearchNumIndex(left, target);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
   }
+  if (arr[middle] === elem) return middle;
+  return -1;
 }
 
 const sorted50 = sortedNumberArray(50);
 
-// clear();
-// t.that(
-//   binarySearchNum(sorted50, 22),
-//   `binarySearchNum(sorted50 , 22)`
-// ).isEquals(true);
-
-// t.that(
-//   binarySearchNum(sorted50, 100),
-//   `binarySearchNum(sorted50 , 100)`
-// ).isEquals(false);
-
 clear();
+t.that(
+  binarySearchNum(sorted50, 22),
+  `binarySearchNum(sorted50 , 22)`
+).isEquals(true);
 
 t.that(
-  binarySearchNumIndex(sorted50, 22),
-  `binarySearchNumIndex(sorted50 , 22)`
-).isEquals(21);
+  binarySearchNum(sorted50, 100),
+  `binarySearchNum(sorted50 , 100)`
+).isEquals(false);
 
-t.that(
-  binarySearchNumIndex(sorted50, 100),
-  `binarySearchNumIndex(sorted50 , 100)`
-).isEquals(-1);
+t.that(BinarySearch(sorted50, 22), `BinarySearch(sorted50 , 22)`).isEquals(21);
+
+t.that(BinarySearch(sorted50, 100), `BinarySearch(sorted50 , 100)`).isEquals(
+  -1
+);
