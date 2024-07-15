b3babe4039a75eca4fa85bfb1ff4ca81d7b9ebc9
YourUsername <your@email.com>
2024-07-15 10:05:03 +0000

    created new branch

---
+++ BigONotation/leetcode/maxProfitAssignment.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[]} difficulty
+ * @param {number[]} profit
+ * @param {number[]} worker
+ * @return {number}
+ */
+var maxProfitAssignment = function (difficulty, profit, worker) {};
