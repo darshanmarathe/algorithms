cf116abd098ea2e44e87f3b3a34782eea26b15b3
YourUsername <your@email.com>
2024-07-15 07:18:15 +0000

    binary search index wip

--- BigONotation/Problems/search/binarySearch.js
+++ BigONotation/Problems/search/binarySearch.js
@@ -1,28 +1,63 @@
-const {log , clear , table} = console;
+const { log, clear, table } = console;
 import Test from "../../../Uitls/test.js";
 var t = new Test("Search Algoriths :: 1 :: Binary search algorithm");
-import { states , sortedNumberArray } from "../data.js";
+import { states, sortedNumberArray } from "../data.js";
 
-function binarySearchNum(arr = [], target= 0) {
-    const len = arr.length;
-    if(len === 0) return false;
-    if(len === 1) return arr[0] === target;
-    const mid  = Math.ceil(len / 2);
-    
-    if(arr[mid] === target) return true;
-    const left = arr.slice(0 , mid);
-    const right = arr.slice(mid , len);
-    
-    if(target > left[left.length -1]){
-        return binarySearchNum(right , target)
-    }else{
-        return binarySearchNum(left , target)
-    }
+function binarySearchNum(arr = [], target = 0) {
+  const len = arr.length;
+  if (len === 0) return false;
+  if (len === 1) return arr[0] === target;
+  const mid = Math.ceil(len / 2);
+
+  if (arr[mid] === target) return true;
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, len);
+
+  if (target > left[left.length - 1]) {
+    return binarySearchNum(right, target);
+  } else {
+    return binarySearchNum(left, target);
+  }
+}
+
+function binarySearchNumIndex(arr = [], target = 0, index = -1) {
+  const len = arr.length;
+  if (len === 0) return -1;
+  if (len === 1) return arr[0] === target ? index : -1;
+  const mid = Math.ceil(len / 2);
+
+  if (arr[mid] === target) return index;
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, len);
+
+  if (target > left[left.length - 1]) {
+    return binarySearchNumIndex(right, target);
+  } else {
+    return binarySearchNumIndex(left, target);
+  }
 }
 
 const sorted50 = sortedNumberArray(50);
 
-clear()
-t.that(binarySearchNum(sorted50 , 22) ,`binarySearchNum(sorted50 , 22)`).isEquals(true);
+// clear();
+// t.that(
+//   binarySearchNum(sorted50, 22),
+//   `binarySearchNum(sorted50 , 22)`
+// ).isEquals(true);
 
-t.that(binarySearchNum(sorted50 , 100) ,`binarySearchNum(sorted50 , 100)`).isEquals(false);
+// t.that(
+//   binarySearchNum(sorted50, 100),
+//   `binarySearchNum(sorted50 , 100)`
+// ).isEquals(false);
+
+clear();
+
+t.that(
+  binarySearchNumIndex(sorted50, 22),
+  `binarySearchNumIndex(sorted50 , 22)`
+).isEquals(21);
+
+t.that(
+  binarySearchNumIndex(sorted50, 100),
+  `binarySearchNumIndex(sorted50 , 100)`
+).isEquals(-1);
