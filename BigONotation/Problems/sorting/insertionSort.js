3e045b2a8f837da81325f0523908a884b45cdb29
YourUsername <your@email.com>
2024-07-24 04:37:04 +0000

    insertion sort done

--- BigONotation/Problems/sorting/insertionSort.js
+++ BigONotation/Problems/sorting/insertionSort.js
@@ -1,26 +1,22 @@
 const { log, clear, table } = console;
 import Test from "../../../Uitls/test.js";
-import { swap } from "./common.js";
 clear();
 var t = new Test("Sorting Algoriths :: 1 :: Insertion sort()");
+// Note :: good for nearly sorted best between bubble sort and selection sort
 
 const array = [64, 34, 25, 12, 22, 15, 11, 44, 33];
 
-const shortArray = [1, 5, 2, 3, 4];
+const shortArray = [1, 5, 2, 4, 3];
 
 const nearlySortedArray = [5, 1, 2, 3, 4];
 
 function insetionSort(arr = []) {
-  for (let i = 1; i < array.length; i++) {
-    const current = array[i];
-    for (let j = 0; j < i; j--) {
-      const element = array[j];
-      if (current < element) {
-        swap(arr, j + 1, j);
-      } else {
-        break;
-      }
+  for (let i = 1; i < arr.length; i++) {
+    const current = arr[i];
+    for (var j = i - 1; j > -1 && arr[j] > current; j--) {
+      arr[j + 1] = arr[j];
     }
+    arr[j + 1] = current;
   }
   return arr;
 }
@@ -30,12 +26,12 @@ t.that(insetionSort(shortArray), `insetionSort(shortArray)`).toBeArray([
   1, 2, 3, 4, 5,
 ]);
 
-// // // [64, 34, 25, 12, 22, 15, 11, 44, 33];
-// t.that(insetionSort(array), `insetionSort(array)`).toBeArray([
-//   11, 12, 15, 22, 25, 33, 34, 44, 64,
-// ]);
+// // [64, 34, 25, 12, 22, 15, 11, 44, 33];
+t.that(insetionSort(array), `insetionSort(array)`).toBeArray([
+  11, 12, 15, 22, 25, 33, 34, 44, 64,
+]);
 
-// t.that(
-//   insetionSort(nearlySortedArray),
-//   `insetionSort(nearlySortedArray)`
-// ).toBeArray([1, 2, 3, 4, 5]);
+t.that(
+  insetionSort(nearlySortedArray),
+  `insetionSort(nearlySortedArray)`
+).toBeArray([1, 2, 3, 4, 5]);
