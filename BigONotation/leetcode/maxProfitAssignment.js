664b914b9cf2ad3111c4c98c71fe677ce5b4307c
Darshan Marathe <darshanmarathe@gmail.com>
2024-07-26 09:33:15 +0000

    work in progress

--- BigONotation/leetcode/maxProfitAssignment.js
+++ BigONotation/leetcode/maxProfitAssignment.js
@@ -4,4 +4,9 @@
  * @param {number[]} worker
  * @return {number}
  */
+
+let difficulty = [2, 4, 6, 8, 10],
+  profit = [10, 20, 30, 40, 50],
+  worker = [4, 5, 6, 7];
+
 var maxProfitAssignment = function (difficulty, profit, worker) {};
