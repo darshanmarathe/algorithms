c02c4c93455fb8844872d7790040119a9aca7ef2
YourUsername <your@email.com>
2024-08-05 05:25:51 +0000

    push and  pop implemented

--- DataStructures/Structures/SinglyLinkList.js
+++ DataStructures/Structures/SinglyLinkList.js
@@ -1,2 +1,64 @@
-class SinglyLinkedList {}
-console.log(SinglyLinkedList.name);
+class Node {
+  next = null;
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  traverse() {
+    var current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+  pop() {
+    if (this.length === 0) return undefined;
+    let current = this.head;
+    let newTail = this.head;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current.val;
+  }
+}
+
+// const node1 = new Node(1);
+// node1.next = new Node(2);
+// node1.next.next = new Node(3);
+// node1.next.next.next = new Node(4);
+
+const linkedList = new SinglyLinkedList();
+linkedList.push(1);
+linkedList.push(2);
+linkedList.push(3);
+linkedList.push(4);
+linkedList.pop();
+console.dir(linkedList);
