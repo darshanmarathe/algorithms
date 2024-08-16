a7e2bf69302921ad628f6b4995d75006e342edfc
Darshan Marathe <darshanmarathe@gmail.com>
2024-08-16 05:08:21 +0000

    Stacks and queues are done

--- DataStructures/Structures/Queues.js
+++ DataStructures/Structures/Queues.js
@@ -1,1 +1,54 @@
-class Queue {}
+const { log, dir, table } = console;
+import Test from "../../Uitls/test.js";
+console.clear();
+var t = new Test("Assignment No :: 1 :: Queues ");
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// FOR FIFO
+export default class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // o(1)
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  // o(1)
+  dequeue() {
+    if (this.size === 0) return null;
+
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    return temp.value;
+  }
+}
+
+const Q = new Queue();
+
+log(Q.enqueue("A"));
+log(Q.enqueue("B"));
+log(Q.enqueue("C"));
+log(Q.dequeue());
+console.log("hello queue", Q);
--- DataStructures/Structures/Stacks.js
+++ DataStructures/Structures/Stacks.js
@@ -1,1 +1,60 @@
-class Stack {}
+const { log, dir, table } = console;
+import Test from "../../Uitls/test.js";
+console.clear();
+var t = new Test("Assignment No :: 1 :: Stacks ");
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// FOR LIFO
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // o(1)
+  push(value) {
+    const newNode = new Node(value);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  //o(1)
+  pop() {
+    if (this.size === 0) {
+      return null;
+    }
+    let temp = this.first;
+    if (this.size === 1 || this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size++;
+
+    return temp.value;
+  }
+}
+
+var stack = new Stack();
+
+stack.push("google.com");
+stack.push("gmail.com");
+stack.push("youtuebe.com");
+stack.push("mobile.youtuebe.com");
+
+stack.pop();
+
+log("hello stacks", stack);
